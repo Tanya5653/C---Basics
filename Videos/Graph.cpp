4265804259b94f8cd784f7f9375a1107c37e045e
Tanya5653 <tanyakumari5653@gmail.com>
2024-04-03 17:18:06 +0530

    added graph

--- Videos/Graph.cpp
+++ Videos/Graph.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 #include<list>
 using namespace std;
  class Graph{
@@ -25,17 +26,55 @@ public:
             cout<<endl;
         }
     }
+    void bfs(int source){
+        queue <int> q;
+        bool *visited = new bool[V]{0};
+
+        q.push(source);
+        visited[source] = true;
+
+        while(!q.empty()){
+           int f= q.front();
+           cout<<f<<endl;
+           q.pop();
+
+           for(auto nbr:l[f]){
+             if(!visited[nbr]){
+                q.push(nbr);
+                visited[nbr] = true;
+             }
+           }
+        }
+    }
+
+    void dfsHelper(int node,bool *visited){
+        visited[node]=true;
+        cout<<node<<endl;
+
+        for(int nbr:l[node]){
+            if(!visited[nbr]){
+                dfsHelper(nbr,visited);
+            }
+        }
+    }
+    void dfs(int source){
+        bool *visited= new bool[V]{0};
+        dfsHelper(source,visited);
+    }
  };
  int main(){
-    Graph g(6);
+    Graph g(7);
     g.addEdge(0,1);
-    g.addEdge(0,4);
-    g.addEdge(2,1);
-    g.addEdge(3,4);
-    g.addEdge(4,5);
+    g.addEdge(1,2);
     g.addEdge(2,3);
     g.addEdge(3,5);
+    g.addEdge(5,6);
+    g.addEdge(4,5);
+    g.addEdge(0,4);
+    g.addEdge(3,4);
 
     g.AdjList();
+   // g.bfs(1);
+    g.dfs(1);
     return 0;
  }
