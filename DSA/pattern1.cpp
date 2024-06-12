cec09f1950d61907c5a5d03b2d185983f823ceb7
Tanya5653 <tanyakumari5653@gmail.com>
2024-06-12 17:03:36 +0530

    pattern added

--- DSA/pattern1.cpp
+++ DSA/pattern1.cpp
@@ -1,8 +1,13 @@
+/*
+* * *
+* * *
+* * *
+*/
 #include<iostream>
 using namespace std;
 // void Pattern(int n){
-//     for(int i=0;i<=n;i++){
-//         for(int j=0;j<=n;j++){
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
 //             cout<<"* ";
 //         }
 //         cout<<endl;
