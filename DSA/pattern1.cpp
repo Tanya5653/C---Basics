e38f2e5ee3477a308f5b0833ae6d5cc33f9ea876
Tanya5653 <tanyakumari5653@gmail.com>
2024-06-12 16:16:34 +0530

    pattren added

--- DSA/pattern1.cpp
+++ DSA/pattern1.cpp
@@ -1,16 +1,26 @@
 #include<iostream>
 using namespace std;
-void Pattern(int n){
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=n;j++){
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
-}
+// void Pattern(int n){
+//     for(int i=0;i<=n;i++){
+//         for(int j=0;j<=n;j++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+// }
 int main(){
     int n;
     cin>>n;
-    Pattern(n);
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<"* ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+   // Pattern(n);
     return 0;
 }
